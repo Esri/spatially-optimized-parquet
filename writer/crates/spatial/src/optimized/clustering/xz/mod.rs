5ff927c573a37214b273b358b60534c907355105
Matt George <mgeorge@esri.com>
2026-07-14 18:39:46 -0700

    refactor: inline z/xz algorithm modules into z.rs/xz.rs
    remove z/algorithm.rs, z/mod.rs, xz/algorithm.rs, xz/mod.rs
    move functions/constants directly into z.rs and xz.rs
    update module imports for clustering module refactor

--- writer/crates/spatial/src/optimized/clustering/xz/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Exposes extent clustering algorithms and typed DataFusion expressions.
-
-mod algorithm;
-mod datafusion;
-
-pub(crate) use algorithm::{DEFAULT_XZ_MAX_LEVEL, extent_xz_code};
-pub(crate) use datafusion::bounds_expr;
-pub(in crate::optimized) use datafusion::non_point_xzcode_from_bounds_expr;
--- writer/crates/spatial/src/optimized/clustering/z/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Exposes point clustering algorithms and typed DataFusion expressions.
-
-mod algorithm;
-mod datafusion;
-
-pub(crate) use algorithm::DEFAULT_COORDINATE_PRECISION;
-pub(crate) use algorithm::point_z_code;
-pub(crate) use datafusion::point_expr;
-pub(in crate::optimized) use datafusion::point_zcode_from_xy_expr;
